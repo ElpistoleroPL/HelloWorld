efb5325d4c326609ad42abc732c8608be92ed9e7
Kamil <kamil_grinholc@o2.pl>
2015-06-23 09:42:48 +0200

    Print 100 times

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,10 @@
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		int i = 100;
+		while(( i--)>0 ){
+			System.out.println("Hello World!!!");
+		}
 	}
 
 }
